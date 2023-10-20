164f4bdab0634d91acc7834e6c7552b203fdff30
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-19 21:09:07 -0300

    feat: add products in db

--- src/main/java/users/usermanagement/data/usecases/CreateProduct.java
+++ src/main/java/users/usermanagement/data/usecases/CreateProduct.java
@@ -1,13 +1,20 @@
 package users.usermanagement.data.usecases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import users.usermanagement.domain.dtos.RequestProduct;
 import users.usermanagement.entities.Product;
+import users.usermanagement.infra.repository.ProductRepository;
 
 @Service
 public class CreateProduct {
+
+    @Autowired
+    private ProductRepository repository;
+
     public Product perform (RequestProduct data) {
         Product product = Product.create(data);
+        repository.save(product);
         return product;
     }
 }
